f1a948ef665c21b8da806aa083f216b7ca40daee
robby-stigler <robbystig@gmail.com>
2019-02-07 01:12:09 -0600

    added hiding functionality to page header

--- abby-hates-brunch/src/Footer.js
+++ abby-hates-brunch/src/Footer.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 
+let footerVals = ["About Us","Contact Us"]
 class Footer extends Component {
     render(){
         return(
             <div className="footer">
+                <div className="footer-value">
+                    {footerVals}
+                </div>
             </div>
         );
     }
--- abby-hates-brunch/src/Header.js
+++ abby-hates-brunch/src/Header.js
@@ -2,9 +2,22 @@ import React, {Component} from 'react';
 import './App.css'
 
 class Header extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {classes: "header"}
+    }
+
+    toggleHide(){
+        if (this.state.classes === "header"){
+            this.setState({classes: "header closed"});
+        } else {
+            this.setState({classes: "header"});
+        }
+    }
     render(){
         return(
-            <div className="header">
+            <div className={this.state.classes} onClick={() => this.toggleHide()}>
                 <p className="header-title"> Abby Hates Brunch</p>
             </div>
         );
