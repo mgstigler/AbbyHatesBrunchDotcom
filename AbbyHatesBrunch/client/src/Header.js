ec18536790473aadff3c1eaaf395e10f226a1760
robby-stigler <robbystig@gmail.com>
2019-02-21 14:40:28 -0600

    blurred header background image

--- AbbyHatesBrunch/client/src/Header.js
+++ AbbyHatesBrunch/client/src/Header.js
@@ -1,27 +1,31 @@
-import React, {Component} from 'react';
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { classes: "header closed" };
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {classes: "header closed"}
+  toggleHide() {
+    if (this.state.classes === "header") {
+      this.setState({ classes: "header closed" });
+    } else {
+      this.setState({ classes: "header closed" });
     }
-
-    toggleHide(){
-        if (this.state.classes === "header"){
-            this.setState({classes: "header closed"});
-        } else {
-            this.setState({classes: "header closed"});
-        }
-    }
-    render(){
-        return(
-            <div className={this.state.classes} onClick={() => this.toggleHide()}>
-                <p className="header-title"> Abby Hates Brunch</p>
-            </div>
-        );
-    }
+  }
+  render() {
+    return (
+      <div className="header" onClick={() => this.toggleHide()}>
+        <div className="header-title">
+          <span>Abby</span>
+          <span style={{ color: "red", margin: "0px 20px" }}>Hates</span>{" "}
+          <span>Brunch</span>
+        </div>
+        <div className="header-image" />
+      </div>
+    );
+  }
 }
 
 export default Header;
